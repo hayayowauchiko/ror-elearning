3e0359546e1d551e16379fff101125f120d8d58f
Yoshikazu <ucchieeee@gmail.com>
2018-05-08 10:12:24 +0800

    create relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,25 @@
 class User < ApplicationRecord
 
-  has_secure_password
-    validates :password,
-            length: {minimum: 6},
-            allow_nil: true
+    has_many :active_relationships, class_name:  "Relationship",
+                                     foreign_key: "follower_id",
+                                     dependent:   :destroy
+    has_many :following, through: :active_relationships, source: :followed
 
+    has_many :passive_relationships, class_name: "Relationship",
+                                     foreign_key: "followed_id",
+                                     dependent: :destroy
+    has_many :followers, through: :passive_relationships
+
+
+    has_secure_password
+        validates :password,
+                        length: {minimum: 6},
+                        allow_nil: true
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # emailを正規表現を使ってvalidates
+    validates :email,
+                    presence: true,
+                    length: {minimum: 5},
+                    format: {with: VALID_EMAIL_REGEX}, #上で定義したvalidを使う
+                    uniqueness: {case_sensitive: false} #大文字も小文字も同じ内容と認識する
 end
