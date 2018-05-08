d75eb91ea498ba004061574ba51cf7c2fbbc5b51
Yoshikazu <ucchieeee@gmail.com>
2018-05-08 12:14:05 +0800

    Add destroy

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-    has_many :words
+    has_many :words, dependent: :destroy
 
     validates :title, presence: true, length: {minimum: 5}
     validates :description, presence: true, length: {minimum: 10}
