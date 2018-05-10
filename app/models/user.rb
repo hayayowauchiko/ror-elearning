d1534929d91790a8a126363ddc7c1aaf4d51aefd
hayayowauchiko <38679521+hayayowauchiko@users.noreply.github.com>
2018-05-10 11:38:47 +0800

    Create signup page (#13)
    * Create signup page
    
    * modify validation and indentation
    
    * Modify email.downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ApplicationRecord
 
     has_many :activities, dependent: :destroy
 
+    before_save :downcase_email #{ email.downcase! }  #callback #before_save {self.email = email.downcase}
+    validates :name, presence: true, length: {minimum: 5}
 
     has_secure_password
         validates :password,
@@ -26,4 +28,9 @@ class User < ApplicationRecord
                     length: {minimum: 5},
                     format: {with: VALID_EMAIL_REGEX}, #上で定義したvalidを使う
                     uniqueness: {case_sensitive: false} #大文字も小文字も同じ内容と認識する
+
+    private
+    def downcase_email
+        email.downcase! #末尾に!をつける事でuser.emailに対しての代入も同時に果たしてくれる
+    end
 end
