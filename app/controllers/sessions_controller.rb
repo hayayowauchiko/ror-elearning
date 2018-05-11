232de4fb4743374a3ee43caf25dfa7efab2d6688
Yoshikazu <ucchieeee@gmail.com>
2018-05-11 20:09:27 +0800

    Modify indentation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,28 @@
 class SessionsController < ApplicationController
   def new
   end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-        #ユーザーログイン後にユーザー情報のページにリダイレクトする
+      if user && user.authenticate(params[:session][:password])
+          #ユーザーログイン後にユーザー情報のページにリダイレクトする
         log_in(user)
-        if params[:session][:remember_me] == '1'
-          remember(user)
-        else
-          forget(user)
-        end
+
+          if params[:session][:remember_me] == '1'
+            remember(user)
+          else
+            forget(user)
+          end
         redirect_to user_url(user) #/users/user.id のget またはredirect_to user_url(user)と同じ
-    else
-        #エラーメッセージ
+      else
+          #エラーメッセージ
         flash.now[:danger] = "invalid email or password"
         render "new"
-    end
+      end
   end
-
+  
   def destroy
     log_out if logged_in?
     redirect_to root_url
   end
-
-
 end
